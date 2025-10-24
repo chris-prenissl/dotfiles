6603af47d7abdb70fd6911a412c5b3320aac571f
Christoh Prenissl <christophprenissl@gmail.com>
2025-10-24 17:49:45 +0200

    Add keymaps for flutter

--- nvim/.config/nvim/lua/plugins/flutter-tools.lua
+++ nvim/.config/nvim/lua/plugins/flutter-tools.lua
@@ -18,5 +18,22 @@ return {
       },
       fvm = true,
     })
+
+    local function setFlutterCommand(character, command, opts)
+      vim.keymap.set("n", "<leader>F" .. character, "<cmd>Flutter" .. command .. "<CR>", opts)
+    end
+
+    local opts = { silent = true }
+    setFlutterCommand("a", "Attach", opts)
+    setFlutterCommand("d", "Debug", opts)
+    setFlutterCommand("e", "Emulators", opts)
+    setFlutterCommand("r", "Run", opts)
+    setFlutterCommand("q", "Quit", opts)
+    setFlutterCommand("h", "Reload", opts)
+    setFlutterCommand("H", "Restart", opts)
+    setFlutterCommand("t", "DevTools", opts)
+    setFlutterCommand("l", "LogToggle", opts)
+    setFlutterCommand("o", "OutlineToggle", opts)
+    setFlutterCommand("p", "PubGet", opts)
   end,
 }
