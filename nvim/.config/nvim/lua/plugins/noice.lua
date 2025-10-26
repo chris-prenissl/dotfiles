399d9eefd41a06420ad6abf37d2d4f30d1c26bad
Christoh Prenissl <christophprenissl@gmail.com>
2025-10-26 17:29:40 +0100

    Center commandline and add mini.diff and mini.files plugin

---
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  opts = {
+    views = {
+      cmdline_popup = {
+        position = {
+          row = 20,
+        },
+      },
+    },
+  },
+}
